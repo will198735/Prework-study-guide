6d3945637536c6ee580c189f7491fe9381fd4e21
will recinos <recinosWilfredo@gmail.com>
2023-08-23 22:42:03 -0700

    added javascript file

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var shape = "circle";
+
+console.log(topics);
+var topics = "HTML, CSS, Git, JavaScript";
+
+var topic = "HTML";
+
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+var shapes = ["triangle", "square", "pentagon", "circle"];
+// console.log(shapes[0]);
+// console.log(shapes[1]);
+// console.log(shapes[2]);
+// console.log(shapes[3]);
+for(var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+   }
